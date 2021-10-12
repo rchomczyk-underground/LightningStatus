2db2044c402bcf891af147d6531f50da21e6625c
shitzuu <ra.chomczyk@gmail.com>
2021-10-12 12:49:52 +0200

    :wrench: Moving from unirest-java library to JDK's 11 HttpClient to reduce final file size.

--- src/main/java/pl/rosesapphire/uptimer/watcher/http/HttpWatcher.java
+++ src/main/java/pl/rosesapphire/uptimer/watcher/http/HttpWatcher.java
@@ -1,9 +1,5 @@
 package pl.rosesapphire.uptimer.watcher.http;
 
-import kong.unirest.HttpRequestWithBody;
-import kong.unirest.HttpResponse;
-import kong.unirest.Unirest;
-import kong.unirest.UnirestException;
 import lombok.RequiredArgsConstructor;
 import pl.rosesapphire.uptimer.config.UptimerConfig;
 import pl.rosesapphire.uptimer.domain.WatchedObject;
@@ -11,7 +7,13 @@ import pl.rosesapphire.uptimer.domain.http.HttpWatchedObject;
 import pl.rosesapphire.uptimer.notifier.Notifier;
 import pl.rosesapphire.uptimer.watcher.SimpleWatcher;
 
-import java.nio.charset.StandardCharsets;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map.Entry;
 import java.util.concurrent.TimeUnit;
@@ -20,25 +22,27 @@ import java.util.concurrent.TimeUnit;
 public class HttpWatcher extends SimpleWatcher<HttpWatchedObject> {
 
     private final UptimerConfig config;
+    private final HttpClient httpClient;
     private final List<Notifier<?, WatchedObject>> notifiers;
 
     @Override
     public void watch(HttpWatchedObject subject) {
-        HttpRequestWithBody httpRequest = Unirest.request(subject.getMethod().name(), subject.getAddress());
-        httpRequest.charset(StandardCharsets.UTF_8);
-        for (Entry<String, String> header : subject.getHeaders().entrySet()) {
-            httpRequest.header(header.getKey(), header.getValue());
+        HttpRequest.Builder httpRequestBuilder = HttpRequest.newBuilder()
+                .uri(URI.create(subject.getAddress()))
+                .method(subject.getMethod().name(), BodyPublishers.noBody());
+        for (Entry<String, String> headerEntry : subject.getHeaders().entrySet()) {
+            httpRequestBuilder.header(headerEntry.getKey(), headerEntry.getValue());
         }
 
-        HttpResponse<?> httpResponse;
+        HttpResponse<String> httpResponse;
         try {
-            httpResponse = httpRequest.asEmpty();
-        } catch (UnirestException exception) {
+            httpResponse = httpClient.send(httpRequestBuilder.build(), BodyHandlers.ofString());
+        } catch (IOException | InterruptedException exception) {
             notifiers.forEach(notifier -> notifier.notifyUnreachable(subject));
             return;
         }
 
-        boolean isAccessible = httpResponse.getStatus() >= subject.getMinimumAcceptedCode() && httpResponse.getStatus() <= subject.getMaximumAcceptedCode();
+        boolean isAccessible = httpResponse.statusCode() >= subject.getMinimumAcceptedCode() && httpResponse.statusCode() <= subject.getMaximumAcceptedCode();
         if (isAccessible) {
             return;
         }
